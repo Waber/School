800612df322f1e49d2c90a3054eecd3150e7ee57
Przemyslaw Waberski <przemekwaberski@gmai.l.com>
2019-05-29 20:39:51 +0200

    first commit

--- src/main/java/com/schoolClass/school/StudentClass.java
+++ src/main/java/com/schoolClass/school/StudentClass.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
@@ -15,13 +16,17 @@ public class StudentClass {
 
     private String studentName;
     List<String> studentList;
-    Files file = new File("C:\\Users\\Standard\\IdeaProjects\\listaUczniow.txt");
+    File file = new File("C:\\Users\\Standard\\IdeaProjects\\listaUczniow.txt");
+    FileReader fileReader;
 
     public StudentClass(String studentName) throws IOException {
         this.studentName = studentName;
 
-        file.createNewFile();
-        studentList = file.rea
+        if(!file.createNewFile()){//metoda zwraca true jeśli utworzy plik, nie jeśli istnieje
+            fileReader = new FileReader(file);
+            studentList = Files.readAllLines(file.toPath());
+        }
+
     }
 
     public void printStudentList(){
